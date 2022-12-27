28662f0e4f9cfabc4b31b29ac979099d854a35dd
fe.hyunsu <fe.hyunsu@gmail.com>
2022-12-27 23:48:47 +0900

    feature : loading update.

--- components/loading/style.tsx
+++ components/loading/style.tsx
@@ -2,7 +2,7 @@ import styled, { keyframes } from 'styled-components';
 
 const loadingMotion = keyframes`
   0%{transform:rotate(0deg);}
-  100%{transform:rotate(360deg);}
+  100%{transform:rotate(-360deg);}
 `;
 
 export const LoadingBox = styled.div`
@@ -14,8 +14,6 @@ export const LoadingBox = styled.div`
   left: 0;
   width: 100%;
   height: 100vh;
-  background-color: #000;
-  opacity: 0.5;
   z-index: 10;
   &:before {
     content: '';
@@ -24,12 +22,22 @@ export const LoadingBox = styled.div`
     right: 0;
     bottom: 0;
     left: 0;
-    width: 3rem;
-    height: 3rem;
+    width: 5rem;
+    height: 5rem;
     margin: auto;
-    border-top: 0.1rem solid #fff;
-    border-right: 0.1rem solid #fff;
+    background: url(images/img_loading.png) 50% 50% / 100% auto;
+    border: 0.2rem solid #fff;
     border-radius: 100%;
-    animation: ${loadingMotion} 1s linear infinite;
+    animation: ${loadingMotion} 5s linear infinite;
+  }
+  &:after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: -1;
   }
 `;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,12 @@ import Loading from '../components/loading';
 import ErrorBox from '../components/error';
 import { useRecoilState } from 'recoil';
 import { locationAtom, headerTitle } from '../store/store';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Autoplay } from 'swiper';
+import 'swiper/swiper.min.css';
 
 const Index = () => {
+  SwiperCore.use([Autoplay]);
   const [isHeaderTitleRecoil, setHeaderTitleRecoil] = useRecoilState(headerTitle);
   const [isLoading, setLoading] = useState<Boolean>(true);
   const [isError, setError] = useState<Boolean>(false);
@@ -37,7 +41,34 @@ const Index = () => {
         <ErrorBox />
       ) : (
         <>
-          <div>거기 지금 어디야</div>
+          <div>
+            <Swiper
+              className="etc-info-slide"
+              direction={'vertical'}
+              autoHeight={true}
+              slidesPerView={1}
+              autoplay={{ delay: 3000 }}
+            >
+              <SwiperSlide>
+                <p>
+                  ☀️ 최고기온 : <span>&nbsp;test</span>
+                </p>
+              </SwiperSlide>
+              <SwiperSlide>
+                <p>
+                  🌥 최저기온 : <span>&nbsp;test2</span>
+                </p>
+              </SwiperSlide>
+              <SwiperSlide>
+                <p>
+                  🧭 풍향 : <span>&nbsp;test3</span>
+                </p>
+              </SwiperSlide>
+              <SwiperSlide>
+                <p>🌊 풍속 : test4</p>
+              </SwiperSlide>
+            </Swiper>
+          </div>
         </>
       )}
     </>
