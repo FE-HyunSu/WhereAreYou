0ee550f437ff89a13b992ee60eceaaab45d6be9c
fe.hyunsu <fe.hyunsu@gmail.com>
2022-12-31 11:29:53 +0900

    feature : intro ui, code cleanup. meta image.

--- components/intro/index.tsx
+++ components/intro/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { IntroUI, BtnShare } from './style';
 import { useRouter } from 'next/router';
 import { useRecoilValue } from 'recoil';
@@ -13,41 +13,15 @@ declare global {
 const IntroView = () => {
   const router = useRouter();
   const recoilLocation = useRecoilValue(locationAtom);
-  const moveTest = () => {
+  const locationSet = () => {
     router.push(`/map?lat=${recoilLocation.lat}&lng=${recoilLocation.lng}`);
   };
 
-  const linkShare = () => {
-    // window.Kakao.init('e30615da0741576c4249e7b7b3dec4ab'); // 내 웹 키를 할당하면 된다
-    // window.Kakao.isInitialized();
-    console.log(window.Kakao);
-    if (window.Kakao) {
-      const kakao = window.Kakao;
-      if (!kakao.isInitialized()) {
-        kakao.init('e30615da0741576c4249e7b7b3dec4ab');
-      }
-      kakao.Share.createCustomButton({
-        container: '#kakaotalk-sharing-btn',
-        templateId: 'e30615da0741576c4249e7b7b3dec4ab',
-        templateArgs: {
-          title: '제목 영역입니다.',
-          description: '설명 영역입니다.',
-        },
-      });
-    }
-  };
-
-  useEffect(() => {
-    if (window.Kakao) {
-      console.log('test');
-      console.log(window.Kakao);
-    }
-  }, []);
   return (
     <>
       <IntroUI>
         <div className="inner">
-          <BtnShare onClick={() => moveTest()}>카카오톡으로 내위치 공유하기</BtnShare>
+          <BtnShare onClick={() => locationSet()}>내위치 공유하기</BtnShare>
         </div>
       </IntroUI>
     </>
--- components/intro/style.tsx
+++ components/intro/style.tsx
@@ -2,16 +2,41 @@ import styled from 'styled-components';
 
 export const IntroUI = styled.div`
   display: flex;
+  position: relative;
   justify-content: center;
   align-items: center;
   width: 100%;
   height: 100%;
+  &:before {
+    content: '';
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    margin: auto;
+    background-color: rgba(255, 255, 255, 0.1);
+    backdrop-filter: blur(2rem);
+    z-index: -1;
+  }
+  &:after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: url(images/img_slide_01.png) no-repeat 50% 50% / cover;
+    z-index: -2;
+  }
   .inner {
     position: relative;
     width: 100%;
-    height: 100%;
-    min-height: 50rem;
-    background: url(images/img_slide_01.png) no-repeat 50% 50% / cover;
+    max-width: 102.4rem;
+    min-height: 100vh;
+    margin: auto;
+    background: url(images/img_slide_01.png) no-repeat 50% 50% / 100% auto;
   }
 `;
 
