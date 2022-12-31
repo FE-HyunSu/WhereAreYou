0284d09471c64dc3198c7c4a3020dee7ea86f1e6
fe.hyunsu <fe.hyunsu@gmail.com>
2022-12-31 23:40:51 +0900

    feature : view page -> Marker.

--- components/kakaomap/view.tsx
+++ components/kakaomap/view.tsx
@@ -14,7 +14,9 @@ const LocationView = () => {
           center={{ lat: Number(recoilLocation.lat), lng: Number(recoilLocation.lng) }}
           style={{ width: '100%', height: '100vh' }}
         >
-          <MarckerItem>나는 지금 여기야</MarckerItem>
+          <MapMarker
+            position={{ lat: Number(recoilLocation.lat), lng: Number(recoilLocation.lng) }}
+          ></MapMarker>
         </Map>
         <BtnShare onClick={() => Router.push('/')}>내 위치도 공유하러 가기</BtnShare>
       </MapUI>
