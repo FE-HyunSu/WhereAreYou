1f4b2b17c94cf710de64dd60c94f42be91954bf7
fe.hyunsu <fe.hyunsu@gmail.com>
2023-04-22 20:54:07 +0900

    feat : recoilPersist 반영.

--- store/store.ts
+++ store/store.ts
@@ -1,19 +1,24 @@
 import { atom } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
 
+const { persistAtom } = recoilPersist();
 export const locationAtom = atom({
   key: 'locationAtom',
   default: {
     lat: 0,
     lng: 0,
   },
+  effects_UNSTABLE: [persistAtom],
 });
 
 export const headerTitle = atom({
   key: 'headerTitle',
   default: '거기 지금 어디야',
+  effects_UNSTABLE: [persistAtom],
 });
 
 export const currentLocation = atom({
   key: 'currentLocation',
   default: '',
+  effects_UNSTABLE: [persistAtom],
 });
